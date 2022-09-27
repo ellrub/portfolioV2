2b533757bc75b8b7cae9d42a7963e1f4562a363e
ellrub <ellefsen88@gmail.com>
2022-09-27 19:20:22 +0200

    Skills & exp |
    Added skills
    Added experience

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -31,7 +31,7 @@ const Skills = () => {
         <h2 className='head-text'>Skills and Experience</h2>
         <div className='app__skills-container'>
             <motion.div className='app__skills-list'>
-                {skills.map((skill) => (
+                {skills?.map((skill) => (
                     <motion.div
                     whileInView={{opacity: [0, 1]}}
                     transition={{duration: 0.5}}
@@ -45,9 +45,47 @@ const Skills = () => {
                     </motion.div>
                 ))}
             </motion.div>
+            <motion.div className='app__skills-exp'>
+                {experience?.map((experience) => (
+                    <motion.div
+                        className='app__skills-exp-item'
+                        key={experience.year}
+                    >
+                        <div className='app__skills-exp-year'>
+                            <p className='bold-text'>{experience.year}</p>
+                        </div>
+                        <motion.div className='app__skills-exp-works'>
+                            {experience.work.map((work) => (
+                                <>
+                                    <motion.div
+                                        whileInView={{opacity: [0, 1]}}
+                                        transition={{duration: 0.5}}
+                                        className="app__skills-exp-work"
+                                        data-tip
+                                        data-for={work.name}
+                                        key={work.name}
+                                    >
+                                        <h4 className='bold-text'>{work.name}</h4>
+                                        <p className='p-text'>{work.company}</p>
+                                    </motion.div>
+                                    <ReactTooltip
+                                        id={work.name}
+                                        effect="solid"
+                                        arrowColor='#fff'
+                                        className='skills-tooltip'
+                                    >
+                                        {work.desc}
+                                    </ReactTooltip>
+                                </>
+                            ))}
+                        </motion.div>
+                    </motion.div>
+                ))}
+                    
+            </motion.div>
         </div>
     </>
 )
 }
 
-export default Skills
+export default AppWrap(Skills, "skills")
