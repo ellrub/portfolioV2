389893bb208a54dbeeacfd25c8d3283ed7f1d1b7
ellrub <ellefsen88@gmail.com>
2023-04-13 18:58:36 +0200

    Update SocialMedia.jsx

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import {BsLinkedin, BsGithub} from "react-icons/bs"
-import {FaFacebookF, FaDiscord} from "react-icons/fa"
+import {FaDiscord} from "react-icons/fa"
 
 const SocialMedia = () => {
     return (
     <div className='app__social'>
         <div className='app__social--icon'>
-            <a href="https://www.linkedin.com/in/ruben-ellefsen/" target="_blank" ><BsLinkedin /></a>
+            <a href="https://www.linkedin.com/in/ruben-ellefsen/" target="_blank" rel="noopener noreferrer" ><BsLinkedin /></a>
         </div>
         <div className='app__social--icon'>
-            <a href="https://github.com/ellrub" target="_blank"><BsGithub /></a>
+            <a href="https://github.com/ellrub" target="_blank" rel="noopener noreferrer"><BsGithub /></a>
         </div>
         <div className='app__social--icon'>
-        <a href="https://discord.com/users/116322935739908097" target="_blank"><FaDiscord /></a>
+        <a href="https://discord.com/users/116322935739908097" target="_blank" rel="noopener noreferrer"><FaDiscord /></a>
         </div>
     </div>
 )
